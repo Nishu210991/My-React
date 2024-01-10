a9ff90b61bd97612693dfda788774a28645f32f4
Nishu Chauhan <nishuchauhan@Nishus-MacBook-Air.local>
2024-01-10 15:50:53 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import "./index.css";
+
+
+const BookList = () => {
+  return <section className='booklist'>
+    <Book />
+    <Book />
+    <Book />
+    <Book />
+
+  </section>
+}
+
+const Book = ()=>{
+  return <article className='book'>
+    <Image />
+    <Title />
+    <Author />
+  </article>
+}
+
+const Image = () => (
+  <img src='./images/book.jpg' 
+  alt='Python All-in-One for Dummies'
+  />
+);
+const Title = () => <h2>Python</h2>
+const Author = () => {
+  return <h4 style={{
+    color: '#617d98',
+    fontSize: '0.75rem',
+    marginTop: '1 rem'
+  }}>John Shovic </h4>
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<BookList />);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
