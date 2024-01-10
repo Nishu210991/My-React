3d9605f86f5f40198afda6c83196635ce9705306
Nishu Chauhan <nishuchauhan@Nishus-MacBook-Air.local>
2024-01-10 19:42:34 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,40 +1,63 @@
-import React from 'react';
+import React, { Children } from 'react';
 import ReactDOM from 'react-dom/client';
 
 import "./index.css";
 
+const firstBook = {
+  author: 'Nishu',
+  title: 'Scorpio',
+  img : './images/book.jpg'
+}
+const secondBook = {
+  author: 'Jeet',
+  title: 'Python',
+  img : './images/python.jpg'
+}
 
 const BookList = () => {
   return <section className='booklist'>
-    <Book />
-    <Book />
-    <Book />
-    <Book />
+    <Book 
+    author = {firstBook.author}
+    title = {firstBook.title}
+    img= {firstBook.img}
+    >
+      <p>Lorem ipsum dolor sit amet consectetur, 
+        adipisicing elit. Quidem, labore fugit? 
+        Optio laudantium sit error amet id quia?</p>
+        <button>click me</button>
+    </Book>
+     <Book 
+    author = {secondBook.author}
+    title = {secondBook.title}
+    img= {secondBook.img}
+    />
+
 
   </section>
 }
 
-const Book = ()=>{
+const Book = (props)=>{
+  const {img, title, author, children} = props
+  console.log(props);
   return <article className='book'>
-    <Image />
-    <Title />
-    <Author />
+    <img src={img} alt={title} />
+    <h2>{title}</h2>
+    <h4>{author}</h4>
+    {children}
   </article>
 }
 
-const Image = () => (
-  <img src='./images/book.jpg' 
-  alt='Python All-in-One for Dummies'
-  />
-);
-const Title = () => <h2>Python</h2>
-const Author = () => {
-  return <h4 style={{
-    color: '#617d98',
-    fontSize: '0.75rem',
-    marginTop: '1 rem'
-  }}>John Shovic </h4>
-}
+// const Book = (props)=>{
+//   console.log(props)
+//   const {img, title, author} = props
+//   return <article className='book'>
+//     <img src={img} alt={props.title} />
+//     <h2>{title}</h2>
+//     <h4>{author}</h4>
+//   </article>
+// }
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<BookList />);
