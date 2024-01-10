f57ec30fdb537a6f23669fcdae66e75a70e7ada1
Nishu Chauhan <nishuchauhan@Nishus-MacBook-Air.local>
2024-01-10 22:50:23 +0530

    44

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { Children } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import "./index.css";
@@ -18,74 +18,37 @@ const books = [
 ];
 
 const BookList = () => {
+  const someValue = "Tokyo"
+  const displayValue = ()=>{
+    console.log(someValue);
+  }
  
   return <section className='booklist'>
-    <EventExample/>
+  
   {books.map((book)=>{
     // const {img, title, author, id} = book
     // return < Book img={img} title={title} author={author} key={id}/>
     // return <Book book={book} key={book.id} />
-    return <Book  {...book} key={book.id} />
+    return <Book  {...book} key={book.id} displayValue={displayValue}/>
 
 
   })};
   </section>
 }
 
-const EventExample = ()=> {
-  const handleFormInput = (e) =>{
-    console.log("Handle form Input");
-    console.log(e);
-    console.log(e.target.name);
-    console.log(e.target.value);
-
-  }
-  const handleButtonClick = () =>{
-    alert("Handle button Click");
-  }
-  const handleSubmitButton = (e) =>{
-    e.preventDefault();
-    console.log("Form Submitted");
-  }
-  return (
-    <section>
-      <form >
-        <h2>Form</h2>
-        <input type="text"
-        name='example'
-        style={{margin:'1rem 0'}} 
-        onChange={handleFormInput}
-        />
-     
-     <div><button type='submit' onClick={handleSubmitButton}>submit </button>
-      <button onClick={handleButtonClick}>click me</button>
-</div>
-</form>
-    </section>
-  )
-
-}
 
 const Book = (props)=>{
-  const {img, title, author, children} = props
+  const {img, title, author, displayValue} = props
   // console.log(props);
+  
   return <article className='book'>
     <img src={img} alt={title} />
     <h2>{title}</h2>
+    <button onClick={displayValue}>display Title</button>
     <h4>{author}</h4>
-    {children}
+    
   </article>
 }
-
-// const Book = (props)=>{
-//   console.log(props)
-//   const {img, title, author} = props
-//   return <article className='book'>
-//     <img src={img} alt={props.title} />
-//     <h2>{title}</h2>
-//     <h4>{author}</h4>
-//   </article>
-// }
 
 
 
