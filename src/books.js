6000fa79cb9286b974faa826a3b924521db6ee9a
Nishu Chauhan <nishuchauhan@Nishus-MacBook-Air.local>
2024-01-11 13:22:04 +0530

    fundamentals

---
+++ src/books.js
@@ -0,0 +1,28 @@
+import img1 from './images/book.jpg'
+import img2 from './images/python.jpg'
+import img3 from './images/book-2.jpg'
+
+
+
+export const books = [
+    {
+     author: 'Nishu',
+     title: 'Scorpio',
+     img : img1,
+     id: 1
+   },
+   {
+     author: 'Jeet',
+     title: 'Python',
+     img : img2,
+     id: 2
+   },
+   {
+    author: 'Dev',
+    title: 'Atomic',
+    img : img3,
+    id: 2
+  }
+   ];
+   
+   
--- src/index.js
+++ src/index.js
@@ -1,55 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import {books}  from './books';
+
 import "./index.css";
-const books = [
- {
-  author: 'Nishu',
-  title: 'Scorpio',
-  img : './images/book.jpg',
-  id: 1
-},
-{
-  author: 'Jeet',
-  title: 'Python',
-  img : './images/python.jpg',
-  id: 2
-}
-];
+import Book from './Book';
 
 const BookList = () => {
-  const someValue = "Tokyo"
-  const displayValue = ()=>{
-    console.log(someValue);
-  }
+  return (
+    <>
+    <h1>Scorpio Best Seller</h1>
+  <section className='booklist'>
  
-  return <section className='booklist'>
-  
-  {books.map((book)=>{
-    // const {img, title, author, id} = book
-    // return < Book img={img} title={title} author={author} key={id}/>
-    // return <Book book={book} key={book.id} />
-    return <Book  {...book} key={book.id} displayValue={displayValue}/>
-
+  {books.map((book, index)=>{
+    return <Book {...book} key={book.id} number={index} />
 
   })};
   </section>
+  </>
+  );
 }
-
-
-const Book = (props)=>{
-  const {img, title, author, displayValue} = props
-  // console.log(props);
-  
-  return <article className='book'>
-    <img src={img} alt={title} />
-    <h2>{title}</h2>
-    <button onClick={displayValue}>display Title</button>
-    <h4>{author}</h4>
-    
-  </article>
-}
-
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
